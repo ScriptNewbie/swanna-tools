8ff08bfdf0d7275f9297af47a9100e22d858415d
Marcin Konopka <marcin.konopka98@protonmail.com>
2024-03-04 14:38:19 +0100

    Print improvements

--- scheduler/src/components/pdfRenderer.tsx
+++ scheduler/src/components/pdfRenderer.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Input } from "@chakra-ui/react";
 import { Liturgy } from "../useLiturgia";
 import {
   days as daysNames,
@@ -7,6 +7,7 @@ import {
 } from "../utils/daysUtils";
 import { MassSchedule } from "../utils/massUtils";
 import { Annoucments } from "./announcement";
+import { useState } from "react";
 
 interface Props {
   startingSunday: Date;
@@ -43,53 +44,74 @@ function PdfRenderer({
   };
 
   const days = getDaysArray(startingSunday);
+
+  const [fontSize, setFontSize] = useState(19);
+
   return (
-    <Flex
-      fontFamily={"Times New Roman"}
-      fontSize={"17pt"}
-      id="print"
-      padding={10}
-      direction="column"
-    >
-      <Box fontWeight="bold" textAlign="center">
-        Porządek nabożeństw <br />
-        {`${getFormatedDate(startingSunday)} – ${getFormatedDate(
-          days[days.length - 1]
-        )}`}
-      </Box>
-      <br />
-      <Box>
-        {days.map((day) => (
-          <Box>
-            <Box fontWeight="bold">{`${
-              daysNames[day.getDay()]
-            } – ${getFormatedDate(day)}${getLiturgyString(day)}`}</Box>
-            {massSchedule[day.toISOString().split("T")[0]]?.map((mass) => (
-              <Box>{`${mass.hour} ${mass.chapel ? "(kaplica) " : ""}${
-                mass.intention
-              }`}</Box>
-            ))}
-          </Box>
-        ))}
-      </Box>
-      <br />
-      <Box id="announcements">
-        <Box textAlign={"center"} fontWeight="bold">
-          Ogłoszenia parafialne z dnia {getFormatedDate(startingSunday)}
+    <>
+      <Flex id="fontSize" mb={3} justifyContent="center" gap={2}>
+        <Input
+          maxWidth="4.5rem"
+          type="number"
+          value={fontSize.toFixed(1)}
+          onChange={(e) => setFontSize(parseFloat(e.target.value))}
+        />
+        <Button onClick={() => setFontSize(fontSize + 0.1)}>+</Button>
+        <Button onClick={() => setFontSize(fontSize - 0.1)}>-</Button>
+      </Flex>
+      <Flex
+        fontFamily={"Times New Roman"}
+        fontSize={fontSize + "pt"}
+        id="print"
+        padding={10}
+        direction="column"
+      >
+        <Box fontWeight="bold" textAlign="center">
+          Porządek nabożeństw <br />
+          {`${getFormatedDate(startingSunday)} – ${getFormatedDate(
+            days[days.length - 1]
+          )}`}
         </Box>
         <br />
-        {announcements[startingSunday.toISOString().split("T")[0]]
-          ?.concat(
-            "Bóg zapłać za wszystkie złożone ofiary oraz za wszelkie wpłaty na konto naszej parafii."
-          )
-          .map((announcement, index) => (
-            <Flex>
-              <Box width="auto">{index + 1 + "."}</Box>
-              <Box textAlign="justify">{announcement}</Box>
-            </Flex>
+        <Box>
+          {days.map((day) => (
+            <Box>
+              <Box fontWeight="bold">{`${
+                daysNames[day.getDay()]
+              } – ${getFormatedDate(day)}${getLiturgyString(day)}`}</Box>
+              {massSchedule[day.toISOString().split("T")[0]]?.map((mass) => (
+                <Flex>
+                  <Box style={{ textWrap: "nowrap" }} min-width="auto">
+                    {`${mass.hour}${mass.chapel ? " (kaplica)" : ""}`}
+                    <Box display="inline" color="white">
+                      -
+                    </Box>
+                  </Box>
+                  <Box textAlign="justify">{`${mass.intention}`}</Box>
+                </Flex>
+              ))}
+            </Box>
           ))}
-      </Box>
-    </Flex>
+        </Box>
+        <br />
+        <Box id="announcements">
+          <Box textAlign={"center"} fontWeight="bold">
+            Ogłoszenia parafialne z dnia {getFormatedDate(startingSunday)}
+          </Box>
+          <br />
+          {announcements[startingSunday.toISOString().split("T")[0]]
+            ?.concat(
+              "Bóg zapłać za wszystkie złożone ofiary oraz za wszelkie wpłaty na konto naszej parafii."
+            )
+            .map((announcement, index) => (
+              <Flex>
+                <Box width="auto">{index + 1 + "."}</Box>
+                <Box textAlign="justify">{announcement}</Box>
+              </Flex>
+            ))}
+        </Box>
+      </Flex>
+    </>
   );
 }
 

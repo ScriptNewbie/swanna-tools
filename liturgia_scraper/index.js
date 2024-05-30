1606624785e8af15ac09f05f7c9d6fb984ec8443
Marcin Konopka <marcin.konopka98@protonmail.com>
2024-05-30 17:40:03 +0200

    Docker

--- liturgia_scraper/index.js
+++ liturgia_scraper/index.js
@@ -15,41 +15,48 @@ const appendLeadingZero = (number) => {
 };
 
 app.get("/", (req, res) => {
-  axios.get("https://niezbednik.niedziela.pl/liturgia").then(({ data }) => {
-    const $ = cheerio.load(data);
-    const extractedData = [];
-    for (let month = 0; month < 12; ++month) {
-      const cards = $("#tab" + appendLeadingZero(month + 1)).find(".card-body");
-      extractedData[month] = [
-        ...cards.map((index, card) =>
-          $(card)
-            .children("a")
-            .children(".kalendarz-year")
-            .children(":nth-child(2)")
-            .children(":nth-child(2)")
-            .text()
-            .replace("\n", "")
-            .replace("\n", "")
-            .split(" ")
-            .filter((str) => str)
-            .join(" ")
-        ),
-      ];
-    }
-    const months = extractedData
-      .map((month, monthIndex) =>
-        month.map((day, dayIndex) => ({
-          date: new Date(
-            `${new Date().getFullYear()}-${appendLeadingZero(
-              monthIndex + 1
-            )}-${appendLeadingZero(dayIndex + 1)}T12:00:00`
+  axios
+    .get("https://niezbednik.niedziela.pl/liturgia")
+    .then(({ data }) => {
+      const $ = cheerio.load(data);
+      const extractedData = [];
+      for (let month = 0; month < 12; ++month) {
+        const cards = $("#tab" + appendLeadingZero(month + 1)).find(
+          ".card-body"
+        );
+        extractedData[month] = [
+          ...cards.map((index, card) =>
+            $(card)
+              .children("a")
+              .children(".kalendarz-year")
+              .children(":nth-child(2)")
+              .children(":nth-child(2)")
+              .text()
+              .replace("\n", "")
+              .replace("\n", "")
+              .split(" ")
+              .filter((str) => str)
+              .join(" ")
           ),
-          description: day,
-        }))
-      )
-      .flat();
-    res.json(months);
-  });
+        ];
+      }
+      const months = extractedData
+        .map((month, monthIndex) =>
+          month.map((day, dayIndex) => ({
+            date: new Date(
+              `${new Date().getFullYear()}-${appendLeadingZero(
+                monthIndex + 1
+              )}-${appendLeadingZero(dayIndex + 1)}T12:00:00`
+            ),
+            description: day,
+          }))
+        )
+        .flat();
+      res.json(months);
+    })
+    .catch((e) => {
+      console.log("Error when downloading content!");
+    });
 });
 
 app.listen(port, () => {
